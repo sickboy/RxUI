4785289b3da43614025c54f5f8ce3e03636ed06e
Kallyn Gowdy <kallyngowdy@gmail.com>
2016-05-22 20:15:11 -0400

    Fix RxApp Scheduler's Import

--- src/rx-app.ts
+++ src/rx-app.ts
@@ -1,13 +1,10 @@
-import {Scheduler as RxScheduler} from "rxjs/Scheduler";
-import {AsapScheduler} from "rxjs/scheduler/AsapScheduler";
-import {QueueScheduler} from "rxjs/scheduler/QueueScheduler";
+import {Scheduler} from "rxjs/Scheduler";
+import {asap} from "rxjs/scheduler/asap";
+import {queue} from "rxjs/scheduler/queue";
 
-// Odd Situation to be in (declaring globals that another library defines)
-// But we should avoid importing values from library wide dependencies (like "rxjs/Rx") to reduce
-// the amount of code that this library requires.
-declare var Scheduler: {
-    asap: AsapScheduler;
-    queue: QueueScheduler;
+let Schedulers = {
+    asap,
+    queue
 };
 
 /**
@@ -18,15 +15,15 @@ export class RxApp {
     /**
      * Gets a scheduler that can be used to scheduler work on the main UI thread.
      */
-    public static get mainThreadScheduler(): RxScheduler {
-        return Scheduler.queue;
+    public static get mainThreadScheduler(): Scheduler {
+        return Schedulers.queue;
     }
     
     /**
      * Gets a scheduler that executes work as soon as it is scheduled.
      */
-    public static get immediateScheduler(): RxScheduler {
-        return Scheduler.asap;
+    public static get immediateScheduler(): Scheduler {
+        return Schedulers.asap;
     }
     
 }
